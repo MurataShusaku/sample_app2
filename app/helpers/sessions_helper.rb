a1e510f96a3a61c182067fd8bfea265e7c1ad350
Shusaku Murata <murata.shayou@gmail.com>
2023-09-20 17:27:14 +0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,28 @@
 module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
+		session[:session_token] = user.session_token
 	end
-	def current_user
-		if session[:user_id]
-			@current_user ||= User.find_by(id: session[:user_id])
+#記憶トークンのcookieに対応するユーザーを返す
+def current_user
+	if (user_id = session[:user_id])
+		user = User.find_by(id: user_id)
+		if user && session[:session_token] == user.session_token
+			@current_user = user
+		end
+	elsif (user_id = cookies.encrypted[:user_id])
+		user = User.find_by(id: user_id)
+		if user && user.authenticated?(cookies[:remember_token])
+			log_in user
+			@current_user = user
 		end
 	end
-	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||=User.find_by(id: user_id)
-		elsif (user_id = cookies.encrypted[:user_id])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
+end
 
-				log_in user
-				@current_user = user
-			end
-		end
+
+	#渡されたユーザーがカレントユーザーであればtrueを返す
+	def current_user?(user)
+		user && user == current_user
 	end
 
 	def logged_in?
@@ -43,4 +48,9 @@ module SessionsHelper
 		reset_session
 		@current_user = nil
 	end
+  # アクセスしようとしたURLを保存する
+	#リクエストが送られたURLをsession変数の:forwarding_url
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
